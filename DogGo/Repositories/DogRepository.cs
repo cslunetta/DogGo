a4088f1aa4f183c4e28f407660ce5ec76235b547
cslunetta <cslunetta@outlook.com>
2021-03-26 16:07:16 -0500

    Implemented Add Edit Delete to Owners and created dog view to list dogs.

---
+++ DogGo/Repositories/DogRepository.cs
@@ -0,0 +1,78 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+    public class DogRepository : IDogRepository
+    {
+        public readonly IConfiguration _config;
+        public DogRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public void AddDog(Dog dog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteDog(Dog dog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Dog> GetAllDogs()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT  d.Id, d.[Name], d.OwnerId, d.Breed, d.Notes, d.ImageUrl
+                          FROM  Dog d
+                                LEFT JOIN Owner o
+                            ON  d.OwnerId = o.Id";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Dog> dogs = new List<Dog>();
+                    while (reader.Read())
+                    {
+                        Dog dog = new Dog
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            OwnerId = reader.GetInt32(reader.GetOrdinal("OwnerId")),
+                            Breed = reader.GetString(reader.GetOrdinal("Breed")),
+                        };
+                        dogs.Add(dog);
+                    }
+                    reader.Close();
+                    return dogs;
+                }
+            }
+        }
+
+        public Dog GetDogById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateDog(Dog dog)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
